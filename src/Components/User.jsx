67b8dbfe2c09f9a91fedd4aaed753afe3123fcc7
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2023-06-28 13:48:08 +0530

    understanding useSearchParams hook

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,11 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link ,useSearchParams } from "react-router-dom";
 
 const User = () => {
+
+  const [searchParams , setSearchParams] = useSearchParams();
+
+  const applyFilter = searchParams.get('filter') === "active";
+
   return (
     <>
        <Link className="user" to='1'>User 1</Link> 
        <Link className="user" to='2'>User 2</Link> 
        <Link className="user" to='3'>User 3</Link> 
+       <div>
+        <button onClick={() => {setSearchParams({filter:"active"})}}>Search Active Users</button>
+        <button onClick={() => {setSearchParams({})}}>Reset</button>
+       </div>
+       {applyFilter ? (<h2>Showing Active Users</h2>) : (<h2>Showing All Users</h2>)}
     </>
     
   )

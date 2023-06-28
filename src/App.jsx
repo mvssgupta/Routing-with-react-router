929211202f932b08a106c1624a8ab7884ed1912a
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2023-06-28 10:20:28 +0530

    added two components and implemented routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
-
+import About from './Components/About'
+import Home from './Components/Home'
+import { BrowserRouter ,Routes , Route } from 'react-router-dom'
 function App() {
 
   return (
     <>
-    <h1>Created React Application using Vite</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+      </Routes>
+    </BrowserRouter>
+    
     </>
   )
 }
---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+const Home = () => {
+
+    return (
+      <>
+        <h1>Home page</h1>
+        <Link to='/about'>About page</Link>
+      </>
+      )
+
+}
+
+export default Home;
